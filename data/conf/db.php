28e45c0786d9c6cccf389f1ed843172fc2cfb30c
tangyongping <yptangecho@163.com>
2016-06-20 18:19:01 +0800

    ss

---
+++ data/conf/db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'yfcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'yf_',
+);
